fc2746896aad032eaf3ba6ad7b099bc6ad90fdee
Isaac Villicana <villicana.i@gmail.com>
2018-06-04 21:05:51 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+
+  def initialize(attributes)
+    attributes.each {|k, v| self.send("#{k}=", v)}
+  end
+
+  def self.save (name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find (id, db)
+    Pokemon.new(id: db.execute("SELECT id FROM pokemon WHERE id = ?", id), name: db.execute("SELECT name FROM pokemon WHERE id = ?", id), type: db.execute("SELECT type FROM pokemon WHERE id = ?", id))
+
+  end
 end
